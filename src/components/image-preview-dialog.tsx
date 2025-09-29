ca13807f5511a55652d3664ba234a23d2cda38fa
Javed Hussain <jvdhussain2008@gmail.com>
2025-09-29 14:29:05 +0000

    Try fixing this error: `Console Error: `DialogContent` requires a `Dialo

--- src/components/image-preview-dialog.tsx
+++ src/components/image-preview-dialog.tsx
@@ -5,6 +5,9 @@ import Image from "next/image";
 import {
   Dialog,
   DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogDescription,
 } from "@/components/ui/dialog";
 
 type ImagePreviewDialogProps = {
@@ -16,6 +19,10 @@ export function ImagePreviewDialog({ imageUrl, onOpenChange }: ImagePreviewDialo
   return (
     <Dialog open={!!imageUrl} onOpenChange={onOpenChange}>
       <DialogContent className="p-0 bg-transparent border-none max-w-4xl w-full h-full flex items-center justify-center">
+        <DialogHeader className="sr-only">
+          <DialogTitle>Image Preview</DialogTitle>
+          <DialogDescription>A full-screen view of the selected image.</DialogDescription>
+        </DialogHeader>
         {imageUrl && (
           <Image
             src={imageUrl}
