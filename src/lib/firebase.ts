7c9af2471ea49aaf0c87f6a66df719aa63d1e3c8
Javed Hussain <jvdhussain2008@gmail.com>
2025-09-29 09:03:21 +0000

    File changes

---
+++ src/lib/firebase.ts
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp, getApp, getApps } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  "projectId": "studio-5333350260-99b15",
+  "appId": "1:622922275285:web:1017cfac15bce8fe2e855c",
+  "apiKey": "AIzaSyBeU6Ww__epaNhRcOuWxB8PUkXIygKnpbc",
+  "authDomain": "studio-5333350260-99b15.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "622922275285"
+};
+
+// Initialize Firebase
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+
+export { app };
