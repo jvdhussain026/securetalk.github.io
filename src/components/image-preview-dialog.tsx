c8ac8e162fb7d1f751d76f5905258b396ab5cf75
Javed Hussain <jvdhussain2008@gmail.com>
2025-09-29 16:27:12 +0000

    hey what you thing i am telling you to make bigger

--- src/components/image-preview-dialog.tsx
+++ src/components/image-preview-dialog.tsx
@@ -41,9 +41,9 @@ export function ImagePreviewDialog({ imagePreview, onOpenChange }: ImagePreviewD
             <Button
                 variant="ghost"
                 size="icon"
-                className="absolute top-6 left-4 z-50 text-white bg-black/30 hover:bg-black/50 hover:text-white rounded-full h-16 w-16"
+                className="absolute top-8 left-6 z-50 text-white bg-black/30 hover:bg-black/50 hover:text-white rounded-full h-20 w-20"
                 >
-                <ArrowLeft className="h-12 w-12" />
+                <ArrowLeft className="h-16 w-16" />
                 <span className="sr-only">Close</span>
             </Button>
         </DialogClose>
