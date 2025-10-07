d176ff0a472b117aa45d15893fec197235b97a0d
Javed Hussain <jvdhussain2008@gmail.com>
2025-10-07 05:31:16 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5333350260-99b15",
+  "appId": "1:622922275285:web:1017cfac15bce8fe2e855c",
+  "apiKey": "AIzaSyBeU6Ww__epaNhRcOuWxB8PUkXIygKnpbc",
+  "authDomain": "studio-5333350260-99b15.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "622922275285"
+};
