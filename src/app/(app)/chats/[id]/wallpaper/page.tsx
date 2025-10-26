f12ed0b07b0bab4f759c5c3f29cf27796ba306e3
Javed Hussain <jvdhussain2008@gmail.com>
2025-10-26 16:06:50 +0000

    Try fixing this error: `Runtime Error: Error: useMemo is not defined. Er

--- src/app/(app)/chats/[id]/wallpaper/page.tsx
+++ src/app/(app)/chats/[id]/wallpaper/page.tsx
@@ -1,7 +1,7 @@
 
 'use client';
 
-import { useState, useEffect, useRef } from 'react';
+import { useState, useEffect, useRef, useMemo } from 'react';
 import { useParams, useRouter } from 'next/navigation';
 import Link from 'next/link';
 import Image from 'next/image';
